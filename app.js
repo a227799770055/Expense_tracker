16ccf0ab63140a554d4083e5b37a6d1fb6c1ec6e
Eddie <a227799770055@gmail.com>
2020-05-19 14:11:55 +0800

    project init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const port = process.env.PORT || 3000
+
+
+// 設定模板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// 設定 middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// 設定路由
+const routes = require("./routes/index")
+app.use(routes)
+
+// 設定監聽
+app.listen(port, () => {
+  console.log(`app is listening at localhost: ${port} `)
+})
