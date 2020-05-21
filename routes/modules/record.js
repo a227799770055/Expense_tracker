8335fcaf14cba758cc33263ae93b219a19e23079
Eddie <a227799770055@gmail.com>
2020-05-21 10:12:23 +0800

    feat: add delete function

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -6,3 +6,12 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 module.exports = router
+
+router.post('/:id/delete', (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  return record.findById(id)
+    .then(record => record.remove())
+    .then(res.redirect('/'))
+    .catch(error => console.log(error))
+})
